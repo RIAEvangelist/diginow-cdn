692026c5a028c26cc50bd3c76c2413c8bac8c519
Brandon Miller <brandon@diginow.it>
2017-08-23 06:34:30 -0700

    initial commit

---
+++ cdn.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const cluster = require('cluster');
+const UglifyJS = require("uglify-js");
+const server=require('node-http-server');
+const numCPUs = require('os').cpus().length;
+
+const config=server.configTemplate();
+config.port             = null;
+config.domain           = 'cdn.diginow.it';
+config.root             = `${__dirname}/node_modules/`;
+config.server.index     = 'index.html';
+config.https            ={
+    privateKey:`/path/to/your/certs/private/server.key`,
+    certificate:`/path/to/your/certs/server.pub`,
+    port:4433,
+    only:true
+}
+
+if (cluster.isMaster) {
+
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+} else {
+
+  server.beforeServe=beforeServe;
+
+  function beforeServe(req,res,body,enc){
+    if(!req.uri.query.min){
+        return;
+    }
+
+    if(res.getHeader('Content-Type')!=server.config.contentType.js){
+          return;
+      }
+
+      body.value=UglifyJS.minify(body.value);
+  }
+
+  server.deploy(config);
+
+}
